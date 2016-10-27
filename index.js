a407b7df97c0a5612d2f9315cb211974fa148263
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2016-10-27 15:22:16 +0700

    Edit wrong logic

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 var program = require('commander');
 
+global.GRAVITY = 10;
 var vx0 = 0, vy0 = 0;
 
 function printPositionAtTime(time) {
   var x = vx0 * time;
-  var y = vy0 * time - (1/2) * vy0 * Math.pow(time, 2);
+  var y = vy0 * time - (1/2) * GRAVITY * Math.pow(time, 2);
 
   console.log("X position: " + x + " meters");
   console.log("Y position: " + y + " meters");
@@ -19,14 +20,12 @@ program
     'Initial X axis velocity (meters) default 0',
     function (value) {
       vx0 = value;
-      console.log(vx0);
     })
  .option(
     '-y, --velocity-y <value>',
     'Initial Y axis velocity (meters) default 0',
     function (value) {
       vy0 = value;
-      console.log(vy0)
     })
  .action(printPositionAtTime)
  .parse(process.argv);
